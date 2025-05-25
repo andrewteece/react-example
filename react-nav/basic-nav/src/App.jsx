2180afcfd7c0cfe3b59474f3fc9e28bb3616b994
andrewteece <andrewteece@gmail.com>
2025-05-25 09:51:53 -0500

    install/setup Vite project with deps

--- react-nav/basic-nav/src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>Basic Nav</h1>;
-}
-
-export default App;
--- react-nav/basic-nav/src/Homepage.jsx
+++
@@ -1,5 +0,0 @@
-function Homepage() {
-  return <h1>Welcome to my basic React site.</h1>;
-}
-
-export default Homepage;
